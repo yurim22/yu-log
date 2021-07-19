42de7d195bdb94c10cee4a1c15847a3b2285cf5a
yurim22 <dek1313@naver.com>
2021-07-20 00:37:07 +0900

    feat: add folder structure

--- src/pages/info.tsx
+++ src/pages/info.tsx
@@ -1,8 +1,10 @@
 import React, { FunctionComponent } from 'react';
 import { graphql } from 'gatsby';
-import Text from 'components/Text';
 import { Link } from 'gatsby';
+import { Global, css } from '@emotion/react';
+import styled from '@emotion/styled';
 
+// dataType 설정
 interface InfoPageProps {
   data: {
     site: {
@@ -15,6 +17,36 @@ interface InfoPageProps {
   };
 }
 
+// style 설정
+const globalStyle = css`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+
+    font-size: 20px;
+  }
+`;
+
+const TextStyle = css`
+  font-size: 18px;
+  font-weight: 700;
+  color: gray;
+`;
+
+const Text1 = styled.div<{ disable: boolean }>`
+  font-sixe: 20px;
+  font-weight: 700;
+  text-decoration: ${({ disable }) => (disable ? 'line-through' : 'none')};
+`;
+
+const Text2 = styled('div')<{ disable: boolean }>(({ disable }) => ({
+  fontSize: '15px',
+  color: 'blue',
+  textDecoration: disable ? 'line-through' : 'none',
+}));
+
+// main funtion
 const InfoPage: FunctionComponent<InfoPageProps> = function ({
   data: {
     site: {
@@ -24,10 +56,11 @@ const InfoPage: FunctionComponent<InfoPageProps> = function ({
 }) {
   return (
     <div>
-      <Text text="Hello, my name is bom2" />
-      <Text text={title} />
-      <Text text={description} />
-      <Text text={author} />
+      <Global styles={globalStyle} />
+      {/* <div css={TextStyle}>{title}</div> */}
+      {title}
+      <Text1 disable={true}>{description}</Text1>
+      <Text2 disable={false}>{author}</Text2>
       <Link to="/">Go to Home</Link>
     </div>
   );
