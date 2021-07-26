145b819ea58693425fbffb1cee93dff3f84078b7
yurim22 <dek1313@naver.com>
2021-07-26 23:29:02 +0900

    feat: 인피티니티 스크롤 기능 구현
    커스텀훅을 이용한 인피니티 스크롤 기능 구현

--- src/components/Main/PostList.tsx
+++ src/components/Main/PostList.tsx
@@ -2,6 +2,9 @@ import React, { FunctionComponent, useMemo } from 'react';
 import styled from '@emotion/styled';
 import PostItem from './PostItem';
 import { FluidObject } from 'gatsby-image';
+import useInfiniteScroll, {
+  useInfiniteScrollType,
+} from 'hooks/useInfiniteScroll';
 
 export type PostType = {
   node: {
@@ -44,23 +47,14 @@ const PostList: FunctionComponent<PostListProps> = function ({
   selectedCategory,
   posts,
 }) {
-  const postListData = useMemo(
-    () =>
-      posts.filter(
-        ({
-          node: {
-            frontmatter: { categories },
-          },
-        }: PostType) =>
-          selectedCategory !== 'All'
-            ? categories.includes(selectedCategory)
-            : true,
-      ),
-    [selectedCategory],
+  const { containerRef, postList }: useInfiniteScrollType = useInfiniteScroll(
+    selectedCategory,
+    posts,
   );
+
   return (
-    <PostListWrapper>
-      {postListData.map(({ node: { id, frontmatter } }: PostType) => (
+    <PostListWrapper ref={containerRef}>
+      {postList.map(({ node: { id, frontmatter } }: PostType) => (
         <PostItem {...frontmatter} link="<https://www.google.co.kr>" key={id} />
       ))}
     </PostListWrapper>
