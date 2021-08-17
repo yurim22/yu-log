659e119a6f5f976bb7bef8cb38f54d7bfbe37064
yurim22 <dek1313@naver.com>
2021-08-17 18:02:30 +0900

    Updates

---
+++ cb1608f2-a2b9d154d9bc2051201f.js
@@ -0,0 +1,3 @@
+/*! For license information please see cb1608f2-a2b9d154d9bc2051201f.js.LICENSE.txt */
+(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[523],{8014:function(c,t,a){"use strict";a.d(t,{acZ:function(){return e}});var e={prefix:"fas",iconName:"arrow-left",icon:[448,512,[],"f060","M257.5 445.1l-22.2 22.2c-9.4 9.4-24.6 9.4-33.9 0L7 273c-9.4-9.4-9.4-24.6 0-33.9L201.4 44.7c9.4-9.4 24.6-9.4 33.9 0l22.2 22.2c9.5 9.5 9.3 25-.4 34.3L136.6 216H424c13.3 0 24 10.7 24 24v32c0 13.3-10.7 24-24 24H136.6l120.5 114.8c9.8 9.3 10 24.8.4 34.3z"]}}}]);
+//# sourceMappingURL=cb1608f2-a2b9d154d9bc2051201f.js.map
